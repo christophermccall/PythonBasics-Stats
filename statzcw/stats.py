fef7fc2ba52e3ee3a355bdb7f59286985b39d8fe
christophermccall <camccall24@gmail.com>
2024-03-22 20:47:14 -0400

    Finished!

--- statzcw/stats.py
+++ statzcw/stats.py
@@ -1,37 +1,51 @@
-
+import math
 from typing import List
 
+
 def zcount(data: List[float]) -> float :
-    pass
+    return len(data)
+
 
 def zmean(data: List[float]) -> float :
-    pass
+    return sum(data)/zcount(data)
 
-def zmode(data: List[float]) -> float :
-    pass
 
-def zmedian(data: List[float]) -> float :
-    pass
+def zmode(data: List[float]) -> float:
+    data_count = [data.count(data[i]) for i in range(len(data))]
+    return data[data_count.index(max(data_count))]
 
-def zvariance(data: List[float]) -> float :
-    pass
-	
-def zstddev(data: List[float]) -> float :
+
+def zmedian(data: List[float]) -> float:
+    return zcount(data)/2
+
+
+def zvariance(data: List[float]) -> float:
+    deviations_squared = [(data[i] - zmean(data))**2 for i in range(len(data))]
+    return sum(deviations_squared) / (zcount(data) - 1)  # the sample variance
+
+
+def zstddev(data: List[float]) -> float:
     # sqrt of variance
-    pass
+    return math.sqrt(zvariance(data))
 
-def zstderr(data: List[float]) -> float :
-    pass
+
+def zstderr(data: List[float]) -> float:
+    return zstddev(data) / math.sqrt(zcount(data))
+
 
 def cov(a, b):
-    pass
+    a_devs = [a[i] - zmean(a) for i in range(len(a))]
+    b_devs = [b[j] - zmean(b) for j in range(len(b))]
+    ab_dev_products = [a_devs[k] * b_devs[k] for k in range(len(a))]
+    return sum(ab_dev_products) / (zcount(a)-1)
 
-def zcorr(datax: List[float], datay: List[float]) -> float :
-    pass
+
+def zcorr(datax: List[float], datay: List[float]) -> float:
+    return cov(datax, datay) / (zstddev(datax) * zstddev(datay))
 
 
 def readDataFile(file):
-    x,y = [], []
+    x, y = [], []
     with open(file) as f:
         first_line = f.readline() # consume headers
         for l in f:
